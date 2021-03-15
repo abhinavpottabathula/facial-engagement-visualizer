7f5ac2845bca7996e69fab954d1e3af29493d651
Abhinav Pottabathula <apottabathula@berkeley.edu>
2021-03-15 13:04:49 -0700

    Added webcam and connected to flask backend for inference

--- server/app.js
+++ server/app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 var cors = require("cors");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var lslRouter = require("./routes/lsl");
 
 var app = express();
 
@@ -23,7 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/lsl", lslRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
