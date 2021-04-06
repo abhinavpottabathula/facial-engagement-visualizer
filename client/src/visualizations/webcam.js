23b3197a5d45f73c4a56f96864c7b1a89f1ba434
Abhinav Pottabathula <apottabathula@berkeley.edu>
2021-04-06 13:57:34 -0700

    Updating graph with emotional states

--- client/src/visualizations/webcam.js
+++ client/src/visualizations/webcam.js
@@ -5,15 +5,51 @@ const SAMPLING_RATE = 5000; // Calculate engagement score every 5 seconds
 
 const Webcam = () => {
     var state = {
-        line1: {
+        angry: {
             x: [],
             y: [], 
-            name: 'LSL',
+            name: 'angry',
+            line: {shape: 'spline'}
+        },
+        disgust: {
+            x: [],
+            y: [], 
+            name: 'disgust',
+            line: {shape: 'spline'}
+        },
+        fear: {
+            x: [],
+            y: [], 
+            name: 'fear',
+            line: {shape: 'spline'}
+        },
+        happy: {
+            x: [],
+            y: [], 
+            name: 'happy',
+            line: {shape: 'spline'}
+        },
+        sad: {
+            x: [],
+            y: [], 
+            name: 'sad',
+            line: {shape: 'spline'}
+        },
+        surprise: {
+            x: [],
+            y: [], 
+            name: 'surprise',
+            line: {shape: 'spline'}
+        },
+        neutral: {
+            x: [],
+            y: [], 
+            name: 'neutral',
             line: {shape: 'spline'}
         },
         layout: { 
-            xaxis:{title: 'Time'},
-            yaxis:{title: 'Engagement Score'},
+            xaxis:{title: 'Time (s)'},
+            yaxis:{title: 'Emotions (%)'},
             datarevision: 0,
         },
         revision: 0,
@@ -56,7 +92,8 @@ const Webcam = () => {
 
         return setInterval(() => {
             ctx.drawImage(video, 0, 0, width, height);
-            let img = ctx.getImageData(0, 0, width, height).data;
+            var img = ctx.getImageData(0, 0, width, height).data;
+            img = Array.from(img)
             getScore(JSON.stringify(img));
         }, SAMPLING_RATE);
     };
@@ -86,30 +123,58 @@ const Webcam = () => {
         })
           .then(res => res.json())
           .then(res => {
-            const { line1, layout} = state;
-            var curScore = parseFloat(res['score']);
+            const {angry, disgust, fear, happy, sad, surprise, neutral, layout} = state;
+            console.log(res)
+            // angry, disgust, fear, happy, sad, surprise, neutral
+            // var curScore = parseFloat(res['score']);
             var curTime = parseInt(res['timestamp']);
+            // {'emotion': {'angry': 0.0009087547195010315, 'disgust': 1.1021758579751944e-09, 'fear': 6.784635155360594e-07, 'happy': 76.04783462907855, 'sad': 0.002571880816411465, 'surprise': 0.0024791463829872784, 'neutral': 23.94620192692257}, 'dominant_emotion': 'happy'}
+
             if (curTime != -1) {
-              var i = line1.x.length - 1;
-              while (i >= 0 && line1.x[i] > curTime) {
-                i--;
-              }
-              line1.x.splice(i + 1, 0, curTime);
-              line1.y.splice(i + 1, 0, curScore);
-              line1.line.shape = 'spline';
+                var i = angry.x.length - 1;
+                while (i >= 0 && angry.x[i] > curTime) {
+                    i--;
+                }
+                angry.x.splice(i + 1, 0, curTime);
+                angry.y.splice(i + 1, 0, parseFloat(res['emotion']['angry']));
+                angry.line.shape = 'spline';
+
+                disgust.x.splice(i + 1, 0, curTime);
+                disgust.y.splice(i + 1, 0, parseFloat(res['emotion']['disgust']));
+                disgust.line.shape = 'spline';
+
+                fear.x.splice(i + 1, 0, curTime);
+                fear.y.splice(i + 1, 0, parseFloat(res['emotion']['fear']));
+                fear.line.shape = 'spline';
+
+                happy.x.splice(i + 1, 0, curTime);
+                happy.y.splice(i + 1, 0, parseFloat(res['emotion']['happy']));
+                happy.line.shape = 'spline';
+
+                sad.x.splice(i + 1, 0, curTime);
+                sad.y.splice(i + 1, 0, parseFloat(res['emotion']['sad']));
+                sad.line.shape = 'spline';
+
+                surprise.x.splice(i + 1, 0, curTime);
+                surprise.y.splice(i + 1, 0, parseFloat(res['emotion']['surprise']));
+                surprise.line.shape = 'spline';
+
+                neutral.x.splice(i + 1, 0, curTime);
+                neutral.y.splice(i + 1, 0, parseFloat(res['emotion']['neutral']));
+                neutral.line.shape = 'spline';
             }
             state.revision = state.revision + 1;
             layout.datarevision = state.revision + 1;
         });
         updatePlot(<Plot
             data={[
-                state.line1
+                state.angry, state.disgust, state.fear, state.happy, state.sad, state.surprise, state.neutral
             ]}
             layout={state.layout}
             revision={state.revision}
             graphDiv="graph"
         />)
-      }
+    }
 
     return (
         <div>
